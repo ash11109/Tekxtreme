1b73fa2d13feb3754269c8082f330d2d8fc514c5
ash <ayushcool1275@gmail.com>
2024-06-29 15:44:56 +0530

    first commit

---
+++ constant.js
@@ -0,0 +1,7 @@
+const kalamAPI = "https://pos.kalamitcompany.com/api/";
+const projectID = "kalamitc_Tekextreme" ;
+const appAPI = kalamAPI + projectID + "/" + "index.php?project_id=" + projectID ;
+const imgLink = kalamAPI + "images/";
+const versionAPI = kalamAPI + "versioncontrol.php";
+const version = "1.0.0";
+const appID = "com.firebasetest.hello";
